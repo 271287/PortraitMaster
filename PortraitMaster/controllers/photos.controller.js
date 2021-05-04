5d18a742e465e22cc61ab424d1df7a88c244cec5
Natalia Szymiczek <nat.szymiczek@gmail.com>
2021-05-04 17:15:13 +0200

    Fix security bug

--- PortraitMaster/controllers/photos.controller.js
+++ PortraitMaster/controllers/photos.controller.js
@@ -1,5 +1,5 @@
 const Photo = require('../models/photo.model');
-const Voter = require('../models/voter.model')
+const Voter = require('../models/Voter.model');
 
 /****** SUBMIT PHOTO ********/
 
@@ -12,21 +12,26 @@ exports.add = async (req, res) => {
     if (title && author && email && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
+      if (title.length > 25 && author.length > 50) {
+        throw new Error('Wrong value');
+      }
+      const reTitleAuthor = /^[a-zA-Z ]*$/;
+      if (!reTitleAuthor.test(title && author)) {
+        throw new Error('Wrong value');
+      }
+      const reEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+      if (!reEmail.test(email)) {
+        throw new Error('Wrong value');
+      }
       const fileExt = fileName.split('.').slice(-1)[0];
-      const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      const badStrings = /[<>%\$]/;
-      if (fileExt === 'jpg' || fileExt === 'gif' || fileExt === 'png'
-        && title.length < 25
-        && badStrings.test(title)
-        && author.length < 50
-        && badStrings.test(author)
-        && validEmail.test(email)) {
+      if (fileExt == 'jpg' || fileExt == 'gif' || fileExt == 'png') {
         const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
         await newPhoto.save(); // ...save new photo in DB
         res.json(newPhoto);
       } else {
-        throw new Error('Wrong file input!');
+        throw new Error('Wrong file format');
       }
+
 
     } else {
       throw new Error('Wrong input!');
@@ -55,27 +60,27 @@ exports.loadAll = async (req, res) => {
 exports.vote = async (req, res) => {
 
   try {
-    const user = await Voter.findOne({ user: req.clientIp });
-    if (user) {
-      const newVote = await Voter.findOne({ $and: [{ user: req.clientIp, votes: req.params.id }] });
+    const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+    const voter = await Voter.findOne({ user: req.clientIp });
+    if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
 
-      if (!newVote) {
-        await Voter.updateOne({ user: req.clientIp }, { $push: { votes: [req.params.id] } });
-        const photoToUpdate = await Photo.findOne({ _id: req.params.id });
+    else {
+      if (voter) {
+        if (voter.votes.includes(photoToUpdate._id)) {
+          res.status(500).json(err);
+        } else {
+          voter.votes.push(photoToUpdate._id);
+          photoToUpdate.votes++;
+          photoToUpdate.save();
+          res.send({ message: 'OK' });
+        }
+      } else {
+        const newVoter = new Voter({ user: req.clientIp, votes: [photoToUpdate._id] });
+        await newVoter.save();
         photoToUpdate.votes++;
         photoToUpdate.save();
         res.send({ message: 'OK' });
-      } else {
-        res.status(500).json(err);
       }
-
-    } else {
-      const newVoter = new Voter({ user: req.clientIp, votes: [req.params.id] });
-      await newVoter.save();
-      const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-      photoToUpdate.votes++;
-      photoToUpdate.save();
-      res.send({ message: 'OK' });
     }
   } catch (err) {
     res.status(500).json(err);
